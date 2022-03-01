ed319b236fd9553ea15ea6aad3289367865bf5b5
bensondevs <bensondevs@gmail.com>
2022-03-01 19:59:38 +0700

    Cors security

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -69,6 +69,7 @@ class Kernel extends HttpKernel
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
 
+        'cors' => \App\Http\Middleware\AllowCors::class,
         'one_time_free_plan' => \App\Http\Middleware\OneTimeFreePlan::class,
     ];
 }
--- app/Http/Middleware/AllowCors.php
+++ app/Http/Middleware/AllowCors.php
@@ -16,7 +16,7 @@ class AllowCors
      */
     public function handle(Request $request, Closure $next)
     {
-        header('Access-Control-Allow-Origin:  http://localhost:4200');
+        header('Access-Control-Allow-Origin:  *');
         header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
         header('Access-Control-Allow-Methods:  POST, PUT');
     }
